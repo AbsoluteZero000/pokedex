710687fbe837ae58ae1e2062089c3bb5c09b294d
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-19 22:51:42 +0300

    added command struct

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import "fmt"
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 
 func main() {
-	fmt.Println("Hello world")
+	for {
+		fmt.Print("pokedex >")
+		fmt.Scanln()
+
+		// return map[string]cliCommand{
+		// 	"help": {
+		// 		name:        "help",
+		// 		description: "Displays a help message",
+		// 		callback:    commandHelp,
+		// 	},
+		// 	"exit": {
+		// 		name:        "exit",
+		// 		description: "Exit the Pokedex",
+		// 		callback:    commandExit,
+		// 	},
+		// }
+	}
 }
