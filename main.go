af7a2f50047880e9dbf960520d4cd69991a01d6e
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-20 19:14:07 +0300

    feature: added help and exit commands

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
 }
 
-
+func commandHelp() error {
+	fmt.Println("Welcom to the PokeDex")
+	return nil
+}
+func commandExit() error {
+	fmt.Println("Turning off the PokeDEX...")
+	os.Exit(0)
+	return nil
+}
 func main() {
-	for {
-		fmt.Print("pokedex >")
-		fmt.Scanln()
+	cliCommands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 
-		// return map[string]cliCommand{
-		// 	"help": {
-		// 		name:        "help",
-		// 		description: "Displays a help message",
-		// 		callback:    commandHelp,
-		// 	},
-		// 	"exit": {
-		// 		name:        "exit",
-		// 		description: "Exit the Pokedex",
-		// 		callback:    commandExit,
-		// 	},
-		// }
+	for {
+		fmt.Print("pokedex > ")
+
+		var command string
+
+		scanner := bufio.NewScanner((bufio.NewReader(os.Stdin)))
+
+		if scanner.Scan() {
+			command = scanner.Text()
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+		}
+
+		if cmd, ok := cliCommands[command]; ok {
+			if err := cmd.callback(); err != nil {
+				fmt.Println(err)
+			}
+		}
 	}
 }
